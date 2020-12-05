8bfeb395f00c76af53e322c3e7cbef9b3957c382
Hélio Cardoso R. de Souza <helio@voxtecnologia.com.br>
2020-12-05 17:08:16 -0300

    segundo commit

---
+++ src/Domain/Model/UsuarioAtribuicao.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Criado por Vox Tecnologia.
+ * User: Helio Cardoso <helio@voxtecnologia.com.br>
+ * Date: 05/12/20
+ */
+
+namespace App\Domain\Model;
+
+
+class UsuarioAtribuicao
+{
+
+}
